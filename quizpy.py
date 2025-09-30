560745d4a106c3cf045c8628f309b7d10fb3d259
DimanNj <dimannj137@gmail.com>
2025-09-30 13:26:07 +0700

    Add files via upload

---
+++ quizpy.py
@@ -0,0 +1,7 @@
+print("noabsen=30");print("nama= diman");print("kelas= 11 rpl1");print("sekolah= SMK PGRI 35 SOLJER")
+print("mata pel= ptgm");print("almat=cibodas")
+
+a=10
+b=5
+c=a+b
+print(c=a+b)
