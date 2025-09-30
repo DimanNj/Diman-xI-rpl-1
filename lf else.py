dc7df81c9bdf01b564a2767d1bc038bd89f34874
DimanNj <dimannj137@gmail.com>
2025-09-30 13:20:09 +0700

    Add files via upload

---
+++ lf else.py
@@ -0,0 +1,8 @@
+# Program sederhana: cek nilai lulus atau tidak
+
+nilai = int(input("Masukkan nilai: "))
+
+if nilai >= 75:
+    print("Selamat, kamu LULUS!")
+else:
+    print("Maaf, kamu TIDAK LULUS.")
