72ac635c0e8e08068e0d2b6d9314de29a495a711
DimanNj <dimannj137@gmail.com>
2025-09-30 13:18:50 +0700

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,13 @@
+nama = input("siapa nama anda ?")
+
+if nama=="dinda" : print("kamu manis sekali !!!")
+print(f"terima kasih {nama}")
+
+
+
+
+
+if nama=="agus":
+  print("kamu ganteng abieeeez!")
+  print("kamu juga keren banget!")
+  print(f"terima kasih {nama}")
