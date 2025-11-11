d73d4e9ef9c452b79dd4af07d753ebcadf7cd634
DimanNj <dimannj137@gmail.com>
2025-11-11 13:25:03 +0700

    Add files via upload

---
+++ CL tugas 1 dan 2_diman.py
@@ -0,0 +1,32 @@
+class alatmusik :
+  
+     nama1 = "drum"
+     asal1 = "jawa"
+     carabermain1 = "pelan/keras"
+     terbuat1 = "stick"
+     harga1 = "500rb"
+     warna1 = "hitam"
+
+
+     nama2 = "gitar"
+     asal2 = "bandung"
+     nada2 = "kunci a,b,c,d,e,f,g"
+     harga2 = "200rb"
+     warna2 = "coklat "
+
+alatmusik1 = alatmusik()
+alatmusik2 = alatmusik()
+
+print(alatmusik1.nama1)
+print(alatmusik1.asal1)
+print(alatmusik1.carabermain1)
+print(alatmusik1.terbuat1)
+print(alatmusik1.harga1)
+print(alatmusik1.warna1)
+
+
+print(alatmusik2.nama2)
+print(alatmusik2.asal2)
+print(alatmusik2.nada2)
+print(alatmusik2.warna2)
+print(alatmusik2.harga2)
